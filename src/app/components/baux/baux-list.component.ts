06af85a6deb04542f7133640670e4c3822456bad
unknown <bilel.oueslatti@gmail.com>
2026-09-17 12:08:46 +0200

    loc-13: guard baux and locataires lists for non-owner users like vue-ensemble

--- src/app/components/baux/baux-list.component.ts
+++ src/app/components/baux/baux-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { BailService } from '../../services/bail.service';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-baux-list',
@@ -9,6 +10,7 @@ import { BailService } from '../../services/bail.service';
   templateUrl: './baux-list.component.html',
 })
 export class BauxListComponent {
+  authService = inject(AuthService);
   bailService = inject(BailService);
   baux = this.bailService.bauxResource.value;
   isLoading = this.bailService.bauxResource.isLoading;
